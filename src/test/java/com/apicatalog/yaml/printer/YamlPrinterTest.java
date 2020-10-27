ad526af002a7d1ae8ed23e5586b25d073ffd9350
Filip <filip26@gmail.com>
2020-10-27 21:45:02 +0100

    tests: update package name (jakarta)

--- src/test/java/com/apicatalog/yaml/printer/YamlPrinterTest.java
+++ src/test/java/com/apicatalog/yaml/printer/YamlPrinterTest.java
@@ -14,18 +14,18 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonString;
-import javax.json.JsonValue;
-import javax.json.stream.JsonParser;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import com.apicatalog.yaml.TestDescription;
 import com.apicatalog.yaml.writer.YamlPrintStyle;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+import jakarta.json.stream.JsonParser;
 
 class YamlPrinterTest {
 
